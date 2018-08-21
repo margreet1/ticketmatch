06123e6145910bcf8325db12b1908d3a0fefe74b
Linus Strobel <lps116@ic.ac.uk>
2018-08-21 12:57:58 +0100

    logic for matches index added

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -1,7 +1,10 @@
 class MatchesController < ApplicationController
   def index
+    @matches = policy_scope(Match)
   end
 
   def show
+    @match = Match.find(params[:id])
+    authorize @match
   end
 end
