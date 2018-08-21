9edd5adafe25792ca7dc27c5883e5d08bf9eb3d3
Linus Strobel <lps116@ic.ac.uk>
2018-08-21 14:36:04 +0100

    index and show for match controller completed- backend

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -1,10 +1,12 @@
 class MatchesController < ApplicationController
   def index
-    @matches = policy_scope(Match)
+    matches = policy_scope(Match).to_a
+    @matches = matches.delete_if{ |match| match.date < Date.today }
   end
 
   def show
     @match = Match.find(params[:id])
+    @tickets = @match.match_tickets
     authorize @match
   end
 end
