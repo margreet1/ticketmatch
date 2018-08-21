60921336b8f98a59a1aa4314a2b5cf85a60eafdd
nanik ramchandani <nanikr@gmail.com>
2018-08-21 15:35:35 +0100

    pages controller

--- app/controllers/seats_controller.rb
+++ app/controllers/seats_controller.rb
@@ -1,7 +1,21 @@
 class SeatsController < ApplicationController
+
   def create
+    @seat = Seat.new(seat_params)
+    if @seat.save
+      redirect_to dashboard_path
+    else
+      render 'new'
+    end
   end
 
   def new
+  @seat = Seat.new
   end
+
+  def show
+  end
+
+
+
 end
