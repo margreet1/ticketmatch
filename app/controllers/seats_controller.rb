d9bb18f0cd54080f76d290f08cd0accd0707a2d8
nanik ramchandani <nanikr@gmail.com>
2018-08-21 16:35:14 +0100

    working on seat setting

--- app/controllers/seats_controller.rb
+++ app/controllers/seats_controller.rb
@@ -2,6 +2,7 @@ class SeatsController < ApplicationController
 
   def create
     @seat = Seat.new(seat_params)
+    authorize @seat
     if @seat.save
       redirect_to dashboard_path
     else
@@ -10,12 +11,17 @@ class SeatsController < ApplicationController
   end
 
   def new
-  @seat = Seat.new
+    @seat = Seat.new
+    authorize @seat
   end
 
   def show
   end
 
+  private
+  def seat_params
+    params.require(:seat).permit(:number, :description, :view)
+  end
 
 
 end
